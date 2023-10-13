cd09f929fa7d5ce55103918a4a1319ba1cd23b15
Fpelourson <fede_pelourson@hotmail.com>
2023-10-13 18:20:32 -0300

    Segunda-entrega

--- main.js
+++ main.js
@@ -1,20 +1,76 @@
-let correctas = [2,1,1,2];
+let base_preguntas = readText("base-preguntas.json")
+let interprete_bp = JSON.parse(base_preguntas)
+let pregunta
+let posibles_respuestas
+let btn_correspondiente = [
+    select_id("btn1"),
+    select_id("btn2"),
+    select_id("btn3"),
+    select_id("btn4")
+]
 
-let opcionElegida = [];
+escogerPreguntaAleatoria()
 
-let cantidadCorrectas = 0;
-
-function respuesta(numPregunta, seleccionada){
-    opcionElegida[numPregunta] = seleccionada.value;
+function escogerPreguntaAleatoria() {
+    escogerPregunta(Math.floor(Math.random() * interprete_bp.length))
 }
 
-function corregir(){
-    cantidadCorrectas = 0;
-    for(i=0; i < correctas.length; i++){
-        if(correctas[i] == opcionElegida[i])
-            cantidadCorrectas++;
+function escogerPregunta(n) {
+    pregunta = interprete_bp[n]
+    select_id("pregunta").innerHTML = pregunta.pregunta
+    desordenarRespuestas(pregunta)
+}
+
+
+function desordenarRespuestas(pregunta) {
+    posibles_respuestas = [
+        pregunta.respuesta, 
+        pregunta.incorrecta1, 
+        pregunta.incorrecta2, 
+        pregunta.incorrecta3
+    ]
+    posibles_respuestas.sort(() => Math.random() - 0.5)
+
+    select_id("btn1").innerHTML = posibles_respuestas[0]
+    select_id("btn2").innerHTML = posibles_respuestas[1]
+    select_id("btn3").innerHTML = posibles_respuestas[2]
+    select_id("btn4").innerHTML = posibles_respuestas[3]
+}
+
+function oprimir_btn(i) {
+    if (posibles_respuestas[i] == pregunta.respuesta) {
+        btn_correspondiente[i].style.background = "green";
+    }else{
+        btn_correspondiente[i].style.background = "red";
     }
-
-document.getElementById("resultado").innerHTML = cantidadCorrectas;
+    setTimeout(() => {
+        reiniciar()
+    }, 3000);
 }
 
+function reiniciar(){
+    for (const btn of btn_correspondiente) {
+        btn.style.background = "white";
+    }
+    escogerPreguntaAleatoria()
+}
+
+function select_id(id){
+    return document.getElementById(id)
+}
+
+function style(id){
+    return select_id(id).style
+}
+
+
+function readText(ruta_local) {
+    var texto = null;
+    var xmlhttp = new XMLHttpRequest();
+    xmlhttp.open("GET", ruta_local, false);
+    xmlhttp.send();
+    if (xmlhttp.status == 200) {
+        texto = xmlhttp.responseText;
+    }
+    return texto;
+    }
